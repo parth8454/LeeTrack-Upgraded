71a05a76a6b7d9520b5378075e26ae50c10b4853
parth8454 <parth8454prajapati@gmail.com>
2026-04-15 14:07:04 +0530

    fix routing

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ const port = 9090 || process.env.PORT;
 app.use(bodyparser.json());
 app.use(express.json());
 app.use(cors({
-    origin:"https://lee-track-upgraded.vercel.app",
+    origin:"https://leetrrack.vercel.app",
     methods:['GET','POST','PUT','DELETE'],
     credentials:true
 }));
